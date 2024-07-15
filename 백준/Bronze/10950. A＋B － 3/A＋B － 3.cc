dfb8b76c675b40def2204ca3c6054cffe836839f
김람운 <110807323+ramun2@users.noreply.github.com>
2024-07-15 18:17:16 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T,i,A,B ;
+    cin >> T;
+    for(i=1; i<T+1; i++){
+        cin >> A >> B ;
+        cout << A+B << endl ;
+    }
+    return 0;
+}

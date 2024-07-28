598b5dc21f682c6b13c8b108069992a78f24180d
김람운 <110807323+ramun2@users.noreply.github.com>
2024-07-28 12:11:26 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string s;
+
+	while(true) {
+		getline(cin, s);
+		if(s == "") break;
+
+		cout << s << endl;
+	}
+
+	return 0;
+}
+ 

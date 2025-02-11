599a5ab35b02b3c3d05711cdc460e42e34474efc
김람운 <110807323+ramun2@users.noreply.github.com>
2025-02-11 22:59:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 460 ms, Memory: 113664 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[][] graph;
+    static boolean[] visited;
+    static int N,M;
+    static int answer = 0;
+
+    public static void dfs(int idx) {
+       visited[idx] = true;
+       for (int i = 0; i <= N; i++) {
+            if(visited[i] == false && graph[idx][i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // 0.입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new boolean[N+1][N+1];
+        visited = new boolean[N+1];
+
+        // 1.graph에 연결 정보 채우기
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x][y] = true;
+            graph[y][x] = true;
+        }
+
+        // 2.dfs(재귀함수) 호출
+        for (int i = 0; i <= N; i++) {
+            if(visited[i] == false){
+                dfs(i);
+                answer ++;
+            }
+        }
+
+        // 3.출력
+        System.out.println(answer-1); // 1번 컴퓨터와 연결된 컴퓨터 수
+    }
+}
